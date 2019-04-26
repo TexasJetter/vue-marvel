80cae596218c402048eebda2bc0ff94da21a2265
Jeff Etter <jeff@theetters.com>
2019-04-26 15:21:27 -0500

    Update core.js

--- src/services/core.js
+++ src/services/core.js
@@ -1,4 +1,4 @@
-const MarvelKey = "----enter your API key here -----";
+const MarvelKey = "----enter your Marvel Public API key here -----";
 const MarvelGatewayUrl = "http://gateway.marvel.com:80/v1/public/";
 
 export { MarvelKey, MarvelGatewayUrl}
